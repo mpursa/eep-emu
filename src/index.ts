b893ade8f9e4bd7e6ac5cd88ac0e2aadfe3ec7db
mpursa <m.pretto93@gmail.com>
2025-12-04 17:53:21 +0100

    Fixed imports, changed npm name

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ import { Emu_RH850 } from './rh850';
 import { Emu_V850e2 } from './v850e2';
 
 // Expose constructors.
-exports.Emu_RH850 = Emu_RH850;
-exports.Emu_V850e2 = Emu_V850e2;
+export { Emu_RH850, Emu_V850e2 };
